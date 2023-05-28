b23ab92f936bc4734eca21dd33b74b37c024cfb1
Spi051997 <97428194+Spi051997@users.noreply.github.com>
2023-05-28 12:27:39 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+import Todolist from '../compoents'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
